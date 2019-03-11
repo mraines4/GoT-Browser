3d7b88cd63d07f3fc9b720912678f256d704bed7
Matt Raines <mraines4@gmail.com>
2019-03-11 15:56:31 -0400

    adds characters

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,54 @@
-// populate list of characters
 
-let mainDiv = document.querySelector('[data-chars]');
+const namesList = document.querySelector("[data-names-list");
+const detailsList = document.querySelector("[data-details-list");
+const name = document.querySelector("[data-name");
+const born = document.querySelector("[data-born]");
+const died = document.querySelector("[data-died]");
+const title = document.querySelector("[data-title]");
+const alleg = document.querySelector("[data-alleg]");
+const alias = document.querySelector("[data-alias]");
+const spouse = document.querySelector("[data-spouse]");
+const mom = document.querySelector("[data-mom]");
+const dad = document.querySelector("[data-dad]");
 
 
-function listOfChars (chars) {
-    return chars.map(function (each) {
-        let charName = document.createElement('p');
-        return charName.textContent = each.name;
+// Set up the left hand pane
+function makeNamesButtons(charactersArray){
+    const names = charactersArray.map(function (guy){
+        return guy.name;
+    });
+    names.forEach(function (name){
+            let button = document.createElement("button");
+            button.textContent = name;
+            button.setAttribute("data-target",'');
+            namesList.append(button);
+        });
+}
+makeNamesButtons(characters);
+
+// Fill in the right hand pane
+function respondToClick() {
+    personName = event.target.textContent;
+    console.log(Object.keys(characters[0]));
+    characters.forEach(function(person){
+        if (person.name == personName) {
+            name.textContent = person.name;
+            born.textContent = person.born;
+            died.textContent = person.died;
+            title.textContent = person.titles;
+            alleg.textContent = person.allegiances;
+            alias.textContent = person.aliases;
+            spouse.textContent = person.spouse;
+            mom.textContent = person.mother;
+            dad.textContent = person.father;
+        }
     });
 }
-mainDiv.append(listOfChars(characters))
+function addClickEvent(person) {
+    person.addEventListener('click', respondToClick);
+}
+
+const characterButtons = document.querySelectorAll('[data-target]');
+characterButtons.forEach(addClickEvent);
+
+
